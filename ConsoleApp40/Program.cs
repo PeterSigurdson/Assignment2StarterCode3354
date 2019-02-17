28210c84d098da1c47d8b9bf9c2060d3ba02b49a
peter sigurdson <peter@petersigurdson.net>
2019-02-16 23:32:29 -0500

    commit 1

--- ConsoleApp40/Program.cs
+++ ConsoleApp40/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp40
+class Program
 {
-    class Program
+    static int Recursive(int value, ref int count)
     {
-        static void Main(string[] args)
+        count++;
+        if (value >= 10)
         {
+            // throw new Exception("End");
+            return value;
         }
+        return Recursive(value + 1, ref count);
     }
+
+    static int a = 5;
+
+    static void Main()
+    {
+        Program.RMethod();
+        Console.WriteLine(" {0} ", a);
+    }
+
+    static int RMethod()
+    {
+        a--;
+        if (a == 0) return a; 
+        return a += RMethod();
+    }
+
+
 }
