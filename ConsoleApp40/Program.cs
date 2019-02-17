c05ea25ffcf69df4285d9196891f2eda295eb51d
peter sigurdson <peter@petersigurdson.net>
2019-02-17 01:12:53 -0500

    c 3

--- ConsoleApp40/Program.cs
+++ ConsoleApp40/Program.cs
@@ -2,31 +2,70 @@
 
 class Program
 {
-    static int Recursive(int value, ref int count)
+    static void Main2()
     {
-        count++;
-        if (value >= 10)
-        {
-            // throw new Exception("End");
-            return value;
-        }
-        return Recursive(value + 1, ref count);
+        bool period; // Used as out parameter.
+        bool comma;
+        bool semicolon;
+        const string value = "has period, comma."; // Used as input string.
+
+        TestString(value, out period, out comma, out semicolon);
+
+        Console.WriteLine(value); // Display value.
+        Console.Write("period: "); // Display labels and bools.
+        Console.WriteLine(period);
+        Console.Write("comma: ");
+        Console.WriteLine(comma);
+        Console.Write("semicolon: ");
+        Console.WriteLine(semicolon);
     }
 
-    static int a = 5;
+    static void TestString(string value,
+        out bool period,
+        out bool comma,
+        out bool semicolon)
+    {
+        // Assign all out parameters to false.
+        period = comma = semicolon = false;
+
+        for (int i = 0; i < value.Length; i++)
+        {
+            switch (value[i])
+            {
+                case '.':
+                    {
+                        period = true; // Set out parameter.
+                        break;
+                    }
+                case ',':
+                    {
+                        comma = true; // Set out parameter.
+                        break;
+                    }
+                case ';':
+                    {
+                        semicolon = true; // Set out parameter.
+                        break;
+                    }
+            }
+        }
+    }
+
+
+    static bool Test(out int size)
+    {
+        // This method has an out parameter.
+        size = 1000;
+        return size >= 1000;
+    }
 
     static void Main()
     {
-        Program.RMethod();
-        Console.WriteLine(" {0} ", a);
+        // Declare out parameter directly inside method call.
+        if (Test(out int size))
+        {
+            Console.WriteLine(size);
+        }
     }
-
-    static int RMethod()
-    {
-        a--;
-        if (a == 0) return a; 
-        return a += RMethod();
-    }
-
-
 }
+
