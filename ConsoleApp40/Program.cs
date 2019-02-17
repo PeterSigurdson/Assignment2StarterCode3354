7bac60b7a7f3f289c490bd82d6937382912ab12b
peter sigurdson <peter@petersigurdson.net>
2019-02-17 12:14:17 -0500

    CSD3354 assignment 2 Starter Code 2019W

--- ConsoleApp40/Program.cs
+++ ConsoleApp40/Program.cs
@@ -1,71 +1,74 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main2()
-    {
-        bool period; // Used as out parameter.
-        bool comma;
-        bool semicolon;
-        const string value = "has period, comma."; // Used as input string.
-
-        TestString(value, out period, out comma, out semicolon);
-
-        Console.WriteLine(value); // Display value.
-        Console.Write("period: "); // Display labels and bools.
-        Console.WriteLine(period);
-        Console.Write("comma: ");
-        Console.WriteLine(comma);
-        Console.Write("semicolon: ");
-        Console.WriteLine(semicolon);
-    }
-
-    static void TestString(string value,
-        out bool period,
-        out bool comma,
-        out bool semicolon)
-    {
-        // Assign all out parameters to false.
-        period = comma = semicolon = false;
-
-        for (int i = 0; i < value.Length; i++)
-        {
-            switch (value[i])
-            {
-                case '.':
-                    {
-                        period = true; // Set out parameter.
-                        break;
-                    }
-                case ',':
-                    {
-                        comma = true; // Set out parameter.
-                        break;
-                    }
-                case ';':
-                    {
-                        semicolon = true; // Set out parameter.
-                        break;
-                    }
-            }
-        }
-    }
-
-
-    static bool Test(out int size)
-    {
-        // This method has an out parameter.
-        size = 1000;
-        return size >= 1000;
-    }
-
-    static void Main()
-    {
-        // Declare out parameter directly inside method call.
-        if (Test(out int size))
-        {
-            Console.WriteLine(size);
-        }
+    static void Main() {
+        new ClassSchedule().Run();
     }
 }
+
+class ClassSchedule
+{
+    List<Student> _students = new List<Student>();
+    Stack<Section> _sections = new Stack<Section>();
+    Queue<Class> _classes = new Queue<Class>();
+
+    public void Run()
+    {
+        this.CreateClassList();
+        this.CreateSections();
+        Console.WriteLine(this.CalculateClassAverageGPA());
+    }
+
+    public float CalculateClassAverageGPA()
+    {
+        float totalGPA = 0;
+        foreach (var student in _students){
+            totalGPA += student.gpa;
+        }
+        return totalGPA/_students.Count;
+    }
+
+    public void CreateClassList ()
+    {
+        _students.Add(new Student("Joe", 3.8f));
+    }
+
+    public void CreateSections()
+    {
+        _sections.Push(new Section { programgroup = "CPCT", term = "2", sectionnumber = "5" });
+    }
+
+    public void CreateClasses()
+    {
+        _classes.Enqueue(new Class { classname = "Java", datetime = "MON_AM", roomnumber = "B230" });
+    }
+}
+
+class Student
+{
+    public string name { get; set; }
+    public string studentnumber { get; set; }
+    public float gpa { get; set; }
+    public string programname { get; set; }
+    public string term { get; set; }
+    public string section_id { get; set; } 
+}
+
+class Section
+{
+    public string section_id;
+    public string class_id; 
+}
+
+class Class
+{
+    public string classname { get; set; }
+    public string datetime { get; set; }
+    public string roomnumber { get; set; }
+    public string section_id { get; set; }
+}
+
 
